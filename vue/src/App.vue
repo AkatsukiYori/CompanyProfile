243793ad1c5e44dc748c9d7a3e386489286bedd7
Julius Kayne <juliuskayne07@gmail.com>
2022-04-22 08:31:10 +0700

    mau pull

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,12 +1,15 @@
 <template>
   <Navbar/>
+  <Carousel/>
 </template>
 <script>
 import Navbar from '@/components/Navbar.vue';
+import Carousel from './components/Carousel.vue';
 export default {
   name: "App",
   components: {
     Navbar,
+    Carousel
   }
 }
 </script>

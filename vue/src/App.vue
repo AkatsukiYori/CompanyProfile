03a330e5f88f07a84fc191fca475f8503ed71377
Feryandi0706 <feryandi2303@gmail.com>
2022-04-23 12:59:12 +0700

    fixing mobile responsive, fixing desktop responsive, add tailwind configure screen

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -3,28 +3,21 @@
     <Navbar/>
     <Beranda />
     <TentangKSD />
-    <Gallery/>
-    <FAQs/>
-    <Footer />
+    <Footer/>
   </div>
 </template>
 <script>
 import Navbar from '@/components/Navbar.vue';
 import Beranda from '@/components/Beranda.vue';
 import TentangKSD from '@/components/TentangKSD.vue';
-import FAQs from '@/components/FAQs.vue';
 import Footer from '@/components/Footer.vue';
-import Gallery from '@/components/Gallery.vue';
 export default {
   name: "App",
   components: {
     Navbar,
     Beranda,
     TentangKSD,
-    FAQs,
-    Footer,
-    Gallery,
-    FAQs
-}
+    Footer
+  }
 }
 </script>
--- vue/src/components/Beranda.vue
+++ vue/src/components/Beranda.vue
@@ -1,12 +1,12 @@
 <template>
-    <div class="bg-purple-600 w-full flex flex-wrap relative py-28 px-12 md:px-20">
-        <div class="lg:w-1/2 md:w-full w-full flex justify-center">
-            <img src="../assets/laptop.png" class="lg:w-11/12 h-auto md:w-full sm:w-full" alt="Gambar">
+    <div class="bg-purple-600 w-full flex flex-wrap relative py-28 px-12 md:px-20 xs:py-10">
+        <div class="lg:w-1/2 md:w-full w-full flex justify-center items-center">
+            <img src="../assets/laptop.png" class="lg:w-11/12 h-auto md:w-full sm:w-full xs:w-full" alt="Gambar">
         </div>
         <div class="flex lg:w-1/2 md:w-full sm:w-full w-full flex-wrap items-center">
             <div>
-                <p class="text-white font-semibold text-6xl w-full my-1">Kata Slogan slogan</p>
-                <p class="text-white lg:text-xl md:text-lg sm:text-base">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam et turpis semper, volutpat justo dictum, aliquam orci.</p>
+                <p class="text-white font-semibold text-6xl w-full py-5 xs:text-5xl">Bersatu Kita Teguh</p>
+                <p class="text-white lg:text-xl md:text-lg sm:text-base py-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam et turpis semper, volutpat justo dictum, aliquam orci.</p>
             </div>
         </div>
     </div>

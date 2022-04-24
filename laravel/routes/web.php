efc90efb7634def3c428aac6a136fc509d63be55
alexander11122004 <alexander.then11@gmail.com>
2022-04-24 18:04:53 +0700

    admin

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,6 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TentangController;
+use App\Http\Controllers\FaqsController;
+use App\Http\Controllers\KontakController;
+use App\Http\Controllers\MitraController;
+use App\Http\Controllers\GalleryController;
+use App\Http\Controllers\DashboardController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +21,23 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('admin/home');
 });
+
+// Dashboard
+Route::get('/dashboard',[App\Http\Controllers\DashboardController::class, 'view'])->name('dashboard');
+
+// Tentang Kami
+Route::get('/tentang',[App\Http\Controllers\TentangController::class, 'view'])->name('tentang');
+
+// Faqs
+Route::get('/faqs',[App\Http\Controllers\FaqsController::class, 'view'])->name('faqs');
+
+// Mitra
+Route::get('/mitra',[App\Http\Controllers\MitraController::class, 'view'])->name('mitra');
+
+// Kontak
+Route::get('/kontak',[App\Http\Controllers\KontakController::class, 'view'])->name('kontak');
+
+// Gallery
+Route::get('/gallery',[App\Http\Controllers\GalleryController::class, 'view'])->name('gallery');

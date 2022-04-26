e49e67b6cc41edf17147923b94b574c688059fcc
Julius Kayne <juliuskayne07@gmail.com>
2022-04-26 14:58:46 +0700

    Update: Gallery.vue

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,34 +1,13 @@
 <template>
   <div>
-    <Navbar />
-    <Beranda />
-    <TentangKSD />
-    <Carousel :color1="'#e1a1ed'" :color2="'#ac7df1'" :color3="'#7658f4'" :headertitle="'Products'" :contents="products"/>
-    <Gallery :galleryitems="gallery"/>
-    <Carousel :color1="'#e1a1ed'" :color2="'#ac7df1'" :color3="'#7658f4'" :headertitle="'Mitra'" :contents="mitras"/>
-    <FAQ :contents="faqs"/>
-    <Footer />
+    <router-view />
   </div>
 </template>
 <script>
-import Navbar from '@/components/Navbar.vue';
-import Beranda from '@/components/Beranda.vue';
-import Gallery from '@/components/Gallery.vue';
-import Carousel from '@/components/Carousel.vue';
-import FAQ from '@/components/FAQ.vue';
-import TentangKSD from '@/components/TentangKSD.vue';
-import Footer from '@/components/Footer.vue';
-export default {
-  name: "App",
-  components: {
-    Navbar,
-    Beranda,
-    Gallery,
-    Carousel,
-    FAQ,
-    TentangKSD,
-    Footer,
-    Gallery
-  }
+export default{
+  
 }
 </script>
+<style>
+
+</style>
--- vue/src/components/Gallery.vue
+++ vue/src/components/Gallery.vue
@@ -2,29 +2,32 @@
     <div>
         <p class="text-purple-700 text-4xl font-semibold pt-7 flex justify-center">Galeri</p>
         <div class="flex flex-wrap">
-            <div class="lg:w-1/2 md:w-full sm:w-full p-8">
-                <img ref="leftimage" src="" alt="Gambar" class="w-11/12">
-                <video ref="leftvideo" src="" controls></video>
-                <iframe ref="leftlink" src=""></iframe>
+            <div class="lg:w-1/2 md:w-4/6 sm:w-full p-8 md:flex md:flex-wrap md:justify-center">
+                <div class="m-auto">
+                    <img ref="leftimage" src="" alt="Gambar" class="w-11/12">
+                    <video ref="leftvideo" src="" controls class="w-11/12 md:w-3/4"></video>
+                    <iframe ref="leftlink" src="" class="w-11/12 md:w-3/4"></iframe>
+                </div>
             </div>
-            <div class="flex flex-wrap w-1/2">
-                <div class="lg:w-4/12 md:w-1/2 sm:w-full h-1/3 p-4 hover:gray" v-for="galleryitem in galleryitems.slice(0,9)" :key="galleryitem.id">
-                    <div v-if="galleryitem.type == 'image'">
-                        <img @click="changeGalleryItem(galleryitem.src, galleryitem.type)" :src="galleryitem.src" alt="src" class="">
+            <div class="flex flex-wrap lg:w-1/2 md:w-2/6">
+                <div class="lg:w-1/3 md:w-1/2 sm:w-full h-1/3 p-4 md:flex md:flex-wrap" v-for="galleryitem in galleryitems.slice(0,9)" :key="galleryitem.id">
+                
+                    <div class="p-1 rounded-lg sm:w-full flex flex-wrap items-center border-purple-600 hover:border-4 transition-all duration-100 hover:cursor-pointer ease-in-out" v-if="galleryitem.type == 'image'">
+                        <img class="w-full" @click="changeGalleryItem(galleryitem.src, galleryitem.type)" :src="galleryitem.src" alt="src" >
                     </div>
-                    <div v-else-if="galleryitem.type == 'video'">
+                    <div class="p-1 rounded-lg lg:w-full flex flex-wrap items-center hover:border-purple-600 hover:border-4 transition-all duration-100 hover:cursor-pointer ease-in-out" v-else-if="galleryitem.type == 'video'">
                         <video @click="changeGalleryItem(galleryitem.src, galleryitem.type)" width="320" height="240">
                             <source :src="galleryitem.src+'#t=0.1'" type="video/mp4">
                             Your browser does not support the video tag.
                         </video>
                     </div>
-                    <div v-else-if="galleryitem.type == 'link'">
+                    <div class="p-1 rounded-lg lg:w-full flex flex-wrap items-center hover:bg-gray-300 border-purple-600 hover:border-4 transition-all duration-100 hover:cursor-pointer ease-in-out" v-else-if="galleryitem.type == 'link'">
                         <img @click="changeGalleryItem(galleryitem.src, galleryitem.type)" :src="galleryitem.src" max-width="100%" style="border:0;" showinfo=0 controls=0 autohide=1>
                     </div>
-                    <div class="h-full text-white" v-else>
-                        <div class="w-10/12 h-full flex items-center justify-center rounded-xl" style="background: linear-gradient(to left bottom, #BF00FE, #444)">
-                            More >> 
-                        </div>
+                    <div class="p-1 rounded-lg h-full text-white lg:w-full flex flex-wrap items-center border-purple-600 hover:border-4 hover:bg-gray-300 transition-all duration-100 hover:cursor-pointer ease-in-out" v-else>
+                        <router-link :to="{ name: 'Album' }" class="w-10/12 h-full flex items-center justify-center rounded-xl" style="background: linear-gradient(to left bottom, #ac7df1, #777)">
+                            More >>
+                        </router-link>
                     </div>
                 </div>
                 

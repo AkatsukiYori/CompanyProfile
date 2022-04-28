4622d758145f03f6aea2146641751917354a9a5b
Julius Kayne <juliuskayne07@gmail.com>
2022-04-28 14:00:49 +0700

    Update: API Laravel

--- laravel/app/Http/Controllers/API/GalleryController.php
+++ laravel/app/Http/Controllers/API/GalleryController.php
@@ -3,9 +3,33 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Models\Album;
+use App\Models\AlbumMedia;
 
 class GalleryController extends Controller
 {
-    //
+    public function index(){
+        $gallery = AlbumMedia::all();
+        $maxGallery = $gallery->sortByDesc('created_at')->take(8);
+        $dataGallery = [];
+        foreach($maxGallery as $key => $value){
+            $dataGallery[$key]['id'] = $key;
+            $dataGallery[$key]['kategori'] = $value->kategori;
+            $dataGallery[$key]['name'] = $value->name;
+            if($value->link){
+                $sourceId = explode('/',$value->link);
+                $latestData = $sourceId[count($sourceId) - 1];
+                $dataGallery[$key]['sourceId'] = $latestData;
+            }else{
+                $dataGallery[$key]['sourceId'] = null;
+            }
+            $dataGallery[$key]['link'] = $value->link;
+        }
+        
+        return response()->json($dataGallery, 200);
+    }
+    
+    // public function showAlbums(){
+        
+    // }
 }

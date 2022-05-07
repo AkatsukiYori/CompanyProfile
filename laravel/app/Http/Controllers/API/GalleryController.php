7738612096391dd43a2ae877430f22828562d1b8
Julius Kayne <juliuskayne07@gmail.com>
2022-05-07 10:50:05 +0700

    Update: API Laravel -> GalleryController

--- laravel/app/Http/Controllers/API/GalleryController.php
+++ laravel/app/Http/Controllers/API/GalleryController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\Album;
 use App\Models\AlbumMedia;
 use App\Http\Controllers\API\functions\ImageStructure;
+use Carbon\Carbon;
 
 class GalleryController extends Controller
 {
@@ -26,8 +27,9 @@ class GalleryController extends Controller
             $albums[$key]['title'] = $value->name;
             $albums[$key]['image'] = $value->image[0]->image;
             $albums[$key]['date'] = date_format(date_create(explode(" ", $value->tgl_album)[0]),'j F Y');
+            // $albums[$key]['tanggal'] = Carbon::parse($value->created_at)->format('j F Y');
             $albums[$key]['description'] = $value->deskripsi;
-            $albums[$key]['album_media'] = $img->AlbumReform($value->album_media);
+            $albums[$key]['album_media'] = $img->AlbumReform($value->new_album);
         }
         
         return response()->json($albums, 200);
@@ -35,15 +37,16 @@ class GalleryController extends Controller
     
     public function GetAlbum($name){
         $img = new ImageStructure();
-        $album = Album::orderBy('created_at')->where('name', 'like', '%' . $name . '%')->get();
+        $album = Album::orderBy('created_at','DESC')->where('name', 'like', '%' . $name . '%')->get();
         foreach($album as $key => $value){
             $albums[$key]['id'] = $key;
             $albums[$key]['title'] = $value->name;
             $albums[$key]['image'] = $value->image[0]->image;
             $albums[$key]['date'] = date_format(date_create(explode(" ", $value->tgl_album)[0]),'j F Y');
             $albums[$key]['description'] = $value->deskripsi;
-            $albums[$key]['album_media'] = $img->AlbumReform($value->album_media);
+            $albums[$key]['album_media'] = $img->AlbumReform($value->new_album);
         }
+        
         
         return response()->json($albums, 200);
     }
